d044b99a74add1e97dff2146e1354a3a90456e9c
Thiago <1thiago1henrique@gmail.com>
2025-03-26 09:45:26 -0300

    criação de pastas

---
+++ src/screens/CategoriesScreen.jsx
@@ -0,0 +1,15 @@
+import { FlatList } from "react-native";
+import { CATEGORIES } from '../../data/dummy-data'
+import CategoryGridTile from "../../components/CategoryGridTile";
+
+function  renderCategoryItem(itemData) {
+    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
+}
+
+const CategoriesScreen = () => {
+    return (
+        <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem}/>
+    )
+}
+
+export default CategoriesScreen;
