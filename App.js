00f6f128dc4a622c5e7c9b34673f2cbe479a2916
Thiago <1thiago1henrique@gmail.com>
2025-03-27 18:23:51 -0300

    Adicionado React Navigator

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import CategoriesScreen from "./src/screens/CategoriesScreen";
 import {StatusBar, StyleSheet} from "react-native";
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <CategoriesScreen />
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
     </>
   );
 }
