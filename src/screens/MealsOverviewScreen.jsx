85c6fb314f8caf42379fde068a4caa0c867ee60a
Thiago <1thiago1henrique@gmail.com>
2025-04-08 11:13:22 -0300

    revisando aulas (aula 100)

--- src/screens/MealsOverviewScreen.jsx
+++ src/screens/MealsOverviewScreen.jsx
@@ -1,43 +1,28 @@
-import { MEALS, CATEGORIES } from '../../data/dummy-data'
-import {FlatList, View, StyleSheet, Text} from "react-native";
-import MealItem from "../../components/MealItem";
-import { useLayoutEffect } from "react";
+import { MEALS } from "../../data/dummy-data";
+import { FlatList, View, StyleSheet } from "react-native";
 
-const MealsOverviewScreen = ({ route, navigation }) => {
-
-    useLayoutEffect(() => {
-        const categoryTitle = CATEGORIES.find((category) => category.id === catId).title;
-        navigation.setOptions({
-            title: categoryTitle,
-        })
-    }, [catId, navigation]);
-
+const MealsOverviewScreen = ({ route }) => {
     const catId = route.params.categoryId;
-
-    const displayMeals = MEALS.filter((mealItem) => {
+    const displayedMeals = MEALS.filter((mealItem) => {
         return mealItem.categoryIds.indexOf(catId) >= 0;
-    })
+    });
 
     const renderMealItem = (itemData) => {
-        const item = itemData.item;
-        const mealItemProps = {
-            title: item.title,
-            imageUrl: item.imageUrl,
-            affordability: item.affordability,
-            complexity: item.complexity,
-            duration: item.duration,
-        }
-        return <MealItem {...mealItemProps} />
+        return (
+            <View>
+
+            </View>
+        )
     }
 
     return (
-        <>
-            <View style={container}>
-                <FlatList data={displayMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
-            </View>
-        </>
+        <View style={container}>
+            <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem}/>
+        </View>
     )
 }
+
+export default MealsOverviewScreen
 
 const { container } = StyleSheet.create({
     container: {
@@ -45,5 +30,3 @@ const { container } = StyleSheet.create({
         padding: 16
     }
 })
-
-export default MealsOverviewScreen;
